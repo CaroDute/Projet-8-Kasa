6a7c7b233b081f074a4d8ac967e8635781aedbca
CaroDute <caro.duteurtre@orange.fr>
2024-03-29 18:46:05 +0100

    Début du style de la page erreur

--- pages/Erreur.jsx
+++ pages/Erreur.jsx
@@ -1,10 +1,15 @@
 import Layout from "../src/components/Layout";
 
-function Erreur () {
-  return <Layout>
-    {'error'}
-    
-    </Layout>;
+function Erreur() {
+  return (
+    <Layout>
+      <div>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+        <p>Retourner sur la page d&apos;accueil</p>
+      </div>
+    </Layout>
+  );
 }
 
 export default Erreur;
